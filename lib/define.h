1de65c8c74b305383be837138dc9695fe49a7572
Gabriel Barbosa <gabriel.a07b@gmail.com>
2019-12-06 20:00:20 -0300

    inicio da IA dos inimigos.

--- lib/define.h
+++ lib/define.h
@@ -4,9 +4,8 @@
 //Definições Gerais para regulagem do jogo
 
 bool jogo_rodando = true;
-float distancia; //temporario
 
-#define VEL_MAX_PERSONAGEM 2
+
 #define ACELERACAO 0.5
 #define TAXA_ATRITO 0.05
 #define N_MAX_PROJETEIS 5 //NUMERO MAXIMO DE PROJETEIS POSSIVEIS NO JOGO
